a30418f34d352491cf05fb30832b110adaff357f
Mykola_Matviiv <matviivmykolav@gmail.com>
2017-08-07 00:36:28 +0300

    S&Mitler_(tenth_commit)

--- psd_to_html/S&Mitler/gulpfile.js
+++ psd_to_html/S&Mitler/gulpfile.js
@@ -30,6 +30,7 @@ gulp.task('js', ['common-js'], function() {
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/mmenu/dist/jquery.mmenu.all.js',
 		'app/libs/owl.carousel/owl.carousel.min.js',
+		'app/libs/equalheights/equalheights.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
