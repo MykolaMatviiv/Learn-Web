68ba66026a9289655135507b95c00b50a499bebc
Mykola_Matviiv <matviivmykolav@gmail.com>
2017-08-13 23:16:00 +0300

    S&Mitler_(twelfth_commit_1)

--- psd_to_html/S&Mitler/app/js/common.js
+++ psd_to_html/S&Mitler/app/js/common.js
@@ -60,6 +60,8 @@ $(function() {
 		ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'));
 	});
 
+	$('select').selectize();
+
 //Resize Window
 	function onResize() {
 		$('.carousel-services-content').equalHeights();
