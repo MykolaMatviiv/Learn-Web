16be9c8a6dc2ee666acbc9ad7c7e6bd4893b5d6e
Mykola_Matviiv <matviivmykolav@gmail.com>
2017-09-28 23:22:27 +0300

    Angular(Lesson_3)

--- Angular/Lesson_3/config.js
+++ Angular/Lesson_3/config.js
@@ -7,17 +7,17 @@ System.config({
     },
 
     paths: {
-        'npm:': './node_modules'
+        'npm:': './node_modules/'
     },
 
     map: {
         'app': './src',
 
-        '@angular/core':'npm:@angular/core/bundles/core.und.js',
-        '@angular/common':'npm:@angular/common/bundles/common.und.js',
-        '@angular/compiler':'npm:@angular/compiler/bundles/compiler.und.js',
-        '@angular/platform-browser':'npm:@angular/platform-browser/bundles/platform-browser.und.js',
-        '@angular/platform-browser-dynamic':'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.und.js',
+        '@angular/core':'npm:@angular/core/bundles/core.umd.js',
+        '@angular/common':'npm:@angular/common/bundles/common.umd.js',
+        '@angular/compiler':'npm:@angular/compiler/bundles/compiler.umd.js',
+        '@angular/platform-browser':'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+        '@angular/platform-browser-dynamic':'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
 
         'rxjs': 'npm:rxjs',
         'typescript': 'npm:typescript',
--- Angular/Lesson_3/src/main.ts
+++ Angular/Lesson_3/src/main.ts
@@ -1,5 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//import { enableProdMode } from '@angular/core';
 
+//enableProdMode{}; //Uncoment for production
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
